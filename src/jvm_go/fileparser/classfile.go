72d77a1d1b0658bc3c40954ee7a7062b1e651960
duanji <duanji@mogujie.com>
2017-08-24 09:02:16 +0800

    版本号检查完成

--- src/jvm_go/fileparser/classfile.go
+++ src/jvm_go/fileparser/classfile.go
@@ -38,12 +38,29 @@ func Parse(data []byte) (cf *ClassFile, err error) {
 //从reader中读取信息
 func (self *ClassFile) read(reader *ClassReader) {
 	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+}
+
+//读取并检查版本好
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	fmt.Printf("majorVersion: %d, minorVersion: %d\n", self.majorVersion, self.minorVersion)
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError")
 }
 
 //读取魔数并检查
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
 	self.magic = reader.readUint32()
-	fmt.Printf("magic number is:%X", self.magic)
+	fmt.Printf("magic number is:%X\n", self.magic)
 	if self.magic != 0xCAFEBABE {
 		panic("java.lang.ClassFormatError: illegal magic!")
 	}
