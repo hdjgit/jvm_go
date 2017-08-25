6e0bea1029202af7eb77978aa8ebe68ac289e27c
duanji <duanji@mogujie.com>
2017-08-25 09:27:14 +0800

    线程、栈、帧 初步完成

---
+++ src/jvm_go/rtdata/frame.go
@@ -0,0 +1,14 @@
+package rtdata
+
+type Frame struct {
+	lower        *Frame
+	localVars    LocalVars     //局部变量表
+	operandStack *OperandStack //操作数栈
+}
+
+func NewFrame(maxLocals, maxStack uint) *Frame {
+	return &Frame{
+		localVars:    newLocalVars(maxLocals),
+		operandStack: newOperandStack(maxStack),
+	}
+}
--- src/jvm_go/rtdata/stack.go
+++ src/jvm_go/rtdata/stack.go
@@ -2,3 +2,40 @@ package rtdata
 
 //java虚拟机规范对java虚拟机栈的约束非常宽松，使用链表数据结构来实现java虚拟机
 
+type Stack struct {
+	maxSize uint   //栈的容量
+	size    uint   //栈的当前大小
+	_top    *Frame //栈顶指针
+}
+
+func newStack(maxSize uint) *Stack {
+	return &Stack{maxSize: maxSize}
+}
+
+//把帧推入栈顶
+func (self *Stack) push(frame *Frame) {
+	self.size += 1
+	if self.size > self.maxSize {
+		panic("java.lang.StackOverflowError")
+	}
+	frame.lower = self._top
+	self._top = frame
+}
+
+func (self *Stack) pop() *Frame {
+	if self._top == nil {
+		panic("jvm stack is empty!")
+	}
+	self.size--
+	frameToPop := self._top
+	self._top = self._top.lower
+	frameToPop.lower = nil
+	return frameToPop
+}
+
+func (self *Stack) top() *Frame {
+	if self._top == nil {
+		panic("jvm stack is empty!")
+	}
+	return self._top
+}
